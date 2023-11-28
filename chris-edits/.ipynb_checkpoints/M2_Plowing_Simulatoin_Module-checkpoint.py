d4cc99fbe282d456608a8c95ab9b80b5b862840c
Chris Doehring <doehring@usc.edu>
2023-11-28 16:12:36 -0500

    got rid of the parts I was messing around in

--- chris-edits/.ipynb_checkpoints/M2_Plowing_Simulatoin_Module-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# Kate 
-# Fack by Major
-
-import numpy.random as random
-
-def Fack_Plow_Simulation(Map):
-
-        Map.roads['plow_time'] = random.uniform(low=0, high=12*60, size=len(Map.roads))
