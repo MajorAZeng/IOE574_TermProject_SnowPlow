08ebcd5ed7870a2f10a417f829daf013aaa630df
Yue "Major" Zeng <majorz@umich.edu>
2023-11-28 09:13:37 -0500

    change to run file

--- Major-Revised-Model/Run_Simulation.py
+++ Major-Revised-Model/Run_Simulation.py
@@ -1,35 +1,24 @@
 # This is the Run File
+from M0_Class_Map_Road import Map
+from M1_Trip_Generation_Module import Generate_Trips
+from M2_Plowing_Simulatoin_Module import Fack_Plow_Simulation
 
-# Import tool packages
-import geopandas as gpd
-import networkx as nx
-import numpy as np
-# Import all modeuls
-import 0_Class_Map_Road
-import 1_Trip_Generation_Module
-import 2_Plowing_Simulatoin_Module
-import 3_Speed_Map_Generation_Module
-import 4_Travel_Simulation_Module
-import 5_Post_Processing_Module
-# 
+def Run_Simulation_Once(map_info_path, snow_depth, trip_origin_path, trip_destination_path):
 
-def Run_Simulation_Once():
-
-    # Create Base Map
-    M = Map()
-    M.Experience_Snow_Event()
+    
+    # Create a based map
+    M = Map(map_info_path)
+    # Experience a snow event
+    M.Experience_Snow(snow_depth)
 
     # Generate trips
-    List_Trips = Trip_Generation_Module(M)
+    Generate_Trips(M,trip_origin_path, trip_destination_path)
 
-    # Generate Plowing Sequence
-    List_of_PlowingMaps = Simulate_Plowing(plowing_sequence)
-
-    # Generate Speed Maps
-    List_of_SeedMaps = Simulate_Speed_Map(M, List_of_PlowingMaps)
+    # Simulation Plowing Time
+    Fack_Plow_Simulation(M)
 
     # Simluation all Trips and record
-    Trip_Records = Simulate_All_Trips(List_Trips, List_of_SeedMaps)
+    # 
 
     return ...  # Some record of the simulation
 
