cd02529f1b8eb7c527b2cdb82272cc09b6761dd4
gfitkin <128532871+gfitkin@users.noreply.github.com>
2023-11-28 21:54:30 -0500

    Update M2_Plowing_Simulatoin_Module.py

--- M2_Plowing_Simulatoin_Module.py
+++ M2_Plowing_Simulatoin_Module.py
@@ -2,54 +2,54 @@
 # Fack by Major
 
 from M0_Class_Map_Road import Map
-from datetime import datetime, timedelta
 import random
 import numpy as np
 
-# Load geopackage
-gdf_edges = gpd.read_file('AA_Map_Final.gpkg', layer='edges')
+def run_snowplow_simulation(num_plows, start_nodes, plow_speed, map_instance, duration_minutes):
+    # Ensure there are as many start nodes as plows
+    if len(start_nodes) != num_plows:
+        raise ValueError("Number of start nodes must match the number of plows")
 
-# Create a graph from the edges
-G = nx.Graph()
-for _, row in gdf_edges.iterrows():
-    G.add_edge(row['node1_id'], row['node2_id'], length=row['length'])
+    # Initialize snowplows
+    snowplows = [{'current_node': start_node, 'plowed_edges': []} for start_node in start_nodes]
 
-class Snowplow:
-    def __init__(self, start_node, map_instance, speed=30):
-        self.current_node = start_node
-        self.map = map_instance
-        self.speed = speed
-        self.plowed_edges = []
+    def calculate_travel_time(length, speed):
+        return length / speed
 
-    def calculate_travel_time(self, length):
-        return length / self.speed
-
-    def choose_edge(self):
-        edges = list(self.map.graph.edges(self.current_node, data=True))
-        unplowed_edges = [edge for edge in edges if self.map.roads.loc[self.map.roads['road_id'] == edge[2]['roadid'], 'plow_time'].iloc[0] == np.inf]
+    def choose_edge(current_node):
+        edges = list(map_instance.graph.edges(current_node, data=True))
+        unplowed_edges = [edge for edge in edges if map_instance.roads.loc[map_instance.roads['road_id'] == edge[2]['roadid'], 'plow_time'].iloc[0] == np.inf]
 
         if unplowed_edges:
             chosen_edge = random.choice(unplowed_edges)
+            # print(f"Snowplow at node {current_node}: Choosing unplowed edge {chosen_edge}")
             return chosen_edge
         else:
             if edges:
                 chosen_edge = random.choice(edges)
+                # print(f"Snowplow at node {current_node}: All edges plowed, choosing random edge {chosen_edge}")
                 return chosen_edge
             else:
+                # print(f"Snowplow at node {current_node}: No edges available, stuck")
                 return None
 
-    def plow(self):
-        edge = self.choose_edge()
-        if edge:
-            road_id = edge[2]['roadid']
-            self.map.roads.loc[self.map.roads['road_id'] == road_id, 'plow_time'] = datetime.now()
-            self.map.roads.loc[self.map.roads['road_id'] == road_id, 'inches_of_snow'] = 0
-            self.current_node = edge[1] if self.current_node == edge[0] else edge[0]
-            self.plowed_edges.append((edge[0], edge[1]))
-            travel_time = self.calculate_travel_time(edge[2]['weight'])
-            return travel_time
-        return 0
+    current_time = 0  # Start at time 0
 
+    while current_time < duration_minutes:  # Convert hours to minutes for comparison
+        for plow in snowplows:
+            edge = choose_edge(plow['current_node'])
+            if edge:
+                road_id = edge[2]['roadid']
+                map_instance.roads.loc[map_instance.roads['road_id'] == road_id, 'plow_time'] = current_time
+                map_instance.roads.loc[map_instance.roads['road_id'] == road_id, 'inches_of_snow'] = 0
+                plow['current_node'] = edge[1] if plow['current_node'] == edge[0] else edge[0]
+                plow['plowed_edges'].append((edge[0], edge[1]))
+                travel_time = calculate_travel_time(edge[2]['weight'], plow_speed)
+                current_time += travel_time
+                # print(f"Snowplow moved to node {plow['current_node']}, current time (minutes): {current_time}")
+
+    return snowplows
+    
 # # Define start nodes for snowplows
 # start_node = 133
 # start_nodes = [133]  # Replace with actual start nodes
